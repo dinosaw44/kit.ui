2cb26181bd73c7ed0b775bdfb645f5bfe1de5458
dinosaw44 <173938953+dinosaw44@users.noreply.github.com>
2024-08-02 17:03:11 +0100

    add components

---
+++ components/svelte/tag/TagLabel.svelte
@@ -0,0 +1,19 @@
+<script lang=ts>
+    export let label: string
+    export let color: number | string = "var(--color-accent)"
+
+    $: base = typeof color === 'string' ? color : `hsl(${color}, 100, 50)`
+</script>
+
+<style lang=scss>
+    span {
+        border: solid .05em;
+        border-radius: 1em;
+        display: inline-block;
+        width: fit-content;
+        padding: 0 .5em;
+        text-wrap: nowrap;
+    }
+</style>
+
+<span style:border-color={base}>{label}</span>
