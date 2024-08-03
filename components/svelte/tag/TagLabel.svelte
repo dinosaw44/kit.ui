e4dafa6e7fc7d1c7ae88430b7b8c1c2b509014c4
dinosaw44 <173938953+dinosaw44@users.noreply.github.com>
2024-08-03 20:41:48 +0100

    update tag component

--- components/svelte/tag/TagLabel.svelte
+++ components/svelte/tag/TagLabel.svelte
@@ -1,6 +1,6 @@
 <script lang=ts>
     export let label: string
-    export let color: number | string = "var(--color-accent)"
+    export let color: number | string = "white"
 
     $: base = typeof color === 'string' ? color : `hsl(${color}, 100, 50)`
 </script>
@@ -10,10 +10,23 @@
         border: solid .05em;
         border-radius: 1em;
         display: inline-block;
-        width: fit-content;
+        isolation: isolate;
+        overflow: hidden;
         padding: 0 .5em;
+        position: relative;
         text-wrap: nowrap;
+        width: fit-content;
+        
+        &::before {
+            content: "\00A0";
+            background-color: black;
+            opacity: .75;
+            position: absolute;
+            left: 0;
+            width: 100%;
+            z-index: -1;
+        }
     }
 </style>
 
-<span style:border-color={base}>{label}</span>
+<span style:background-color={base} style:border-color={base}>{label}</span>
